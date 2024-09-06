8931fd329681180590fe0c2d75644c0ae3289943
LuigiJoseph <81258629+LuigiJoseph@users.noreply.github.com>
2024-09-07 01:58:50 +0300

    Time: 35 ms (85.74%), Space: 12.6 MB (44.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,9 +8,9 @@ class Solution(object):
         
         HashMap = {} #val : index
 
-        for index, val in enumerate(nums):
-            difference = target - val
+        for index, val in enumerate(nums): #enumerate allows to iterate and get both index and value at the same time
+            difference = target - val           #check if both the values exit so far in the hashmap
             if difference in HashMap:
-                return [HashMap[difference], index]
-            HashMap[val] = index
+                return [HashMap[difference], index] 
+            HashMap[val] = index # if not, update the index of the hash with the current iterated value and repeat
         return
