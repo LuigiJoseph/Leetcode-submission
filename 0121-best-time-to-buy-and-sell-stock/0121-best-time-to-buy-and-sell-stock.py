c352b0f92ca362f4b90e591455df6dd6bad41bcf
LuigiJoseph <81258629+LuigiJoseph@users.noreply.github.com>
2024-09-07 23:58:03 +0300

    Time: 766 ms (42.25%), Space: 20.7 MB (95.48%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
-        profit = 0
-        buy = prices[0]
+    
+        #2 pointer approach l,r
+        #if r value is bigger than l, move to next indices
 
-        for p in prices:
-            buy = min(buy, p)
-            profit = max(profit, p - buy)
-        
-        return profit
+        l , r = 0, 1 #left = buy, right = sell
+        maxProfit = 0
+
+
+        while r < len(prices):
+            #is it profitable?
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+            #not profitable
+            else:
+                l = r
+            r += 1
+        return maxProfit
+                
+  
